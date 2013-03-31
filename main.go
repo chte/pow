3c3612bc6129b907d654cd48adb0ce198f77894c
Christopher Teljstedt <chte@kth.se>
2013-03-31 16:37:45 +0200

    Added attacker page

--- main.go
+++ main.go
@@ -12,18 +12,33 @@ import (
 
 var addr = flag.String("addr", ":8080", "http service address")
 var homeTempl = template.Must(template.ParseFiles("./home.html"))
+var attackTempl = template.Must(template.ParseFiles("./attack.html"))
 var jsTempl = template.Must(template.ParseFiles("./pow.js"))
+var jsAtkTempl = template.Must(template.ParseFiles("./pow_atk.js"))
+var jsAtkTaskTempl = template.Must(template.ParseFiles("./attacktask.js"))
 
 type homeParam struct {
 	Difficulty int
 	Problems   int
 }
 
-var defaultParam = &homeParam{2, 256}
+var defaultParam = &homeParam{3, 16}
 
 func homeHandler(c http.ResponseWriter, req *http.Request) {
 	homeTempl.Execute(c, defaultParam)
 }
+func attackHandler(c http.ResponseWriter, req *http.Request) {
+	attackTempl.Execute(c, req.Host)
+}
+
+func jsAtkHandler(c http.ResponseWriter, req *http.Request) {
+	jsAtkTempl.Execute(c, req.Host)
+}
+
+func jsAtkTaskHandler(c http.ResponseWriter, req *http.Request) {
+	jsAtkTaskTempl.Execute(c, req.Host)
+}
+
 func jsHandler(c http.ResponseWriter, req *http.Request) {
 	jsTempl.Execute(c, req.Host)
 }
@@ -34,6 +49,10 @@ func main() {
 	// go h.run()
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/pow.js", jsHandler)
+	http.HandleFunc("/attack", attackHandler)
+	http.HandleFunc("/pow_atk.js", jsAtkHandler)
+	http.HandleFunc("/attacktask.js", jsAtkTaskHandler)
+
 	http.Handle("/ws", websocket.Handler(wsHandler))
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
---
+++ pow_atk.js
@@ -0,0 +1,83 @@
+/**
+ * An implemention of the proof-of-work protocol with
+ * a puzzle called reverse compute hash.
+ * This code solves the puzzle Reverse compute hash.
+ * 
+ * The code is in the public domain.
+ * Written by: Felix Leopoldo Rios and Christian Hellman
+ */
+
+$(document).ready(function(){
+    /* 
+     * This search function is protected by proof-of-work.
+     */
+    $("#attackbtn").click(function(){
+        var numWorkers = parseInt($('#number_of_attacker_field').val());
+        startWorkerSwarm(numWorkers)
+    });
+});
+
+
+/*************************** Web worker logics ********************************/
+/* 
+ * Start webworkers
+ */
+function startWorkerSwarm(numWorkers){
+	var conn;
+	var sockets = [];
+	// Initial setup.
+		for(var i = 0; i < numWorkers; i++){
+			(function() {
+				if (window["WebSocket"]) {
+					conn = new WebSocket("ws://{{$}}/ws");
+					sockets.push(conn);
+
+			        conn.onclose = function(evt) {  
+			           log("Connection closed.");
+			        };
+			        conn.onmessage = function(evt) {
+			            // alert("Got response: " + evt.data);
+			            var response = JSON.parse(evt.data);
+			            var index = i;
+			            if(response["Opcode"] == 1){
+
+			                // alert("Problems is:" + response.Problems);
+			                w = new Worker("attacktask.js");
+			                w.onmessage=function (e){
+								 var worker_data=e.data;
+								 var solution = e.data.solution; 
+								 // recieved data from worker
+								 // alert(worker_data);
+								//alert(JSON.stringify(solution));
+							
+			                // $("#result").append("<br/>" + solution + "<br/>");
+			                var request = { "Problems": solution, 
+			                                "Query": response.Query,
+			                                "Hash": CryptoJS.SHA256(solution + "" + response["Seed"]).toString(CryptoJS.enc.Hex),
+			                                "Opcode": 1};
+			                //alert(JSON.stringify(request))
+			                conn.send(JSON.stringify(request));
+			           		}
+			           		//Send message with data to worker
+			           		w.postMessage({id: index, problems: response["Problems"], difficulty: response["Difficulty"]});
+			            } 
+			        }
+			        conn.onopen = function(evt) {
+			        	var request = {"Opcode": 0, "Query": "Calle"};
+		     			this.send(JSON.stringify(request));
+			        }
+			  	}
+		    })();
+
+		}
+}
+
+
+function log(msg){
+	 $("#msg").append("<br/><b>"+msg+"</b><br/>");
+}
+function append_result(str){
+	$("#result").append(str);
+}
+
+
