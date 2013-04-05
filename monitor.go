eee0d24802bee5b664bf98564b330b013fc39785
Christopher Teljstedt <chte@kth.se>
2013-04-05 13:54:20 +0200

    Fixed a bug with regex not extracting CPU utilization correctly on Ubuntu

--- monitor.go
+++ monitor.go
@@ -59,7 +59,7 @@ func collect(ch chan information) {
 		cmd = exec.Command("top", "-n", "0", "-stats", "cpu", "-l", "0") //Mac
 	} else {
 		//cmd = exec.Command("sar", "-u", "1")
-		cmd = exec.Command("top", "-b", "-d", "1" )
+		cmd = exec.Command("top", "-b", "-d", "0.75")
 	}
 
 	stdout, err := cmd.StdoutPipe()
@@ -81,24 +81,23 @@ func collect(ch chan information) {
 	} else {
 
 		//exp, _ = regexp.Compile("all\\s+(\\S+)\\s+\\S+\\s+(\\S+)")
-		exp, _ = regexp.Compile("\\s(.*?)%us,  (.*?)%sy")
+		exp, _ = regexp.Compile("\\s (.*?) us,  (.*?) sy")
 	}
 	// exp, _ := regexp.Compile("(\\d\\.\\d{2})") //Ubuntu
 	//exp, _ := regexp.Compile("CPU usage: (.*?)% user, (.*?)% sys, (.*?)% idle") //Mac
 	for {
 		line, _, _ := rd.ReadLine()
-		// fmt.Printf("%s\n", line)
+		//fmt.Printf("%s\n", line)
 
 		// catch := exp.FindAllString(string(line), 3) //Ubuntu
 		catch := exp.FindStringSubmatch(string(line)) //Mac
 		if catch != nil {
-			
+			//log.Printf("%s\n", catch[0])
 
 			// user, _ := strconv.ParseFloat(catch[0], 64) //Ubuntu
 			user, _ := strconv.ParseFloat(catch[1], 64)
 			system, _ := strconv.ParseFloat(catch[2], 64)
-			log.Printf("%s\n", catch[0])
-			log.Printf("%f(%s) %f\n",user, catch[1], system)
+			//log.Printf("%f(%s), %f(%s)\n", user, catch[1], system, catch[2])
 			ch <- information{Cpu_user: user, Cpu_system: system, Monitoring: len(h.connections), Users: connections, ShortAverageTime: globalAccess.ShortMean, LongAverageTime: globalAccess.LongMean}
 			CPU_LOAD = user
 		}
