46786f361a4577e54409f4651bcaebe8a9dffcd4
Fredrik Lilkaer <lilkaer@kth.se>
2013-04-08 15:57:44 +0200

    Alpha of dataminer

--- dataminer/dataminer.go
+++ dataminer/dataminer.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 	"text/template"
-	"time"
+	// "time"
 )
 
 var controlTempl = template.Must(template.ParseFiles("./dataminer/control.html"))
@@ -14,8 +14,14 @@ func controlHTMLHandler(c http.ResponseWriter, req *http.Request) {
 	controlTempl.Execute(c, req.Host)
 }
 
+// var datacollection = {    "Behaviour": behaviour_type, 
+//         "RequestingTime": requestingTime,
+//         "SolvingTime": solvingTime,
+//         "GrantingTime": grantingTime};
+
 type sample struct {
-	Tid time.Time
+	Behaviour                                    string
+	RequestingTime, SolvingingTime, GrantingTime int
 }
 
 type ctrlMsg struct {
@@ -32,6 +38,37 @@ var h = hub{
 	log:     make(chan sample, 100),
 }
 
+type Data struct {
+	series map[string]*dataseries
+}
+
+func (d *Data) getSeries(s string) *dataseries {
+	if d.series[s] == nil {
+		d.series[s] = NewDataSeries()
+	}
+	return d.series[s]
+}
+
+type dataseries struct {
+	data []sample
+}
+
+func NewDataSeries() *dataseries {
+	d := new(dataseries)
+	d.data = make([]sample, 0, 100)
+	return d
+}
+func (d *dataseries) add(s sample) {
+	d.data = append(d.data, s)
+}
+func NewData() *Data {
+	var d Data
+	d.series = make(map[string]*dataseries)
+	return &d
+}
+
+var data *Data
+
 func (h *hub) run() {
 	for {
 		select {
@@ -39,14 +76,17 @@ func (h *hub) run() {
 			switch cmd.Command {
 			case 0:
 				log.Printf("Start recording\n")
+				data = NewData()
 				break
 			case 1:
 				log.Printf("Stop Recording save to file\n")
+				log.Printf("Data: %v", data)
 				break
 			}
 			break
 		case smp := <-h.log:
 			log.Printf("Received sample %v\n", smp)
+			data.getSeries(smp.Behaviour).add(smp)
 		}
 	}
 }
